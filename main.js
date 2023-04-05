0a546f75387eb06fb34434f7622906aa1c4b3b54
jugoncalves13 <129890076+jugoncalves13@users.noreply.github.com>
2023-04-05 11:56:46 -0300

    nova alteração

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener( "click" , showcolor);
 
 document.getElementById( "botao" ).addEventListener( "click" , exibenome );
 
+document.getElementById( "btn-somar" ).addEventListener( "click" , soma );
+
 function showcolor()
 {
     document.body.style.backgroundColor ="red";
@@ -11,4 +13,10 @@ function exibenome()
     let usuario = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerText = "meu nome e:" + usuario
     document.getElementById( "nome" ).value = "";
+}
+function soma()
+{
+    let num1 = document.getElementById( "num1" ).value;
+    let num2 = document.getElementById( "num2" ).value;
+    document.getElementById( "resultado1" ).innerText = num1 + num2; 
 }
