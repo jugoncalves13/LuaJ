ff307691027139d130e48e034f32ad53f1f741e2
jugoncalves13 <129890076+jugoncalves13@users.noreply.github.com>
2023-04-05 10:54:40 -0300

    alterações

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 document.addEventListener( "click" , showcolor);
 
+document.getElementById( "botao" ).addEventListener( "click" , exibenome );
+
 function showcolor()
 {
     document.body.style.backgroundColor="red";
+}
+function exibenome()
+{
+    
 }
